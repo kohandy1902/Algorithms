c395889e39ea52a2763370cf09c16a6f16976f1e
Donghyun Koh <andy1902@snu.ac.kr>
2022-02-21 17:27:09 +0900

    Change Tab to Space

--- Z.cpp
+++ Z.cpp
@@ -26,47 +26,47 @@ int z[101010];
 
 void Z()
 {
-	int l = 0, r = 0;
-	for(int i = 1; i < s.size(); i++)
-	{
-		if(r < i)
-		{
-			for(int j = i; j < s.size(); j++)
-			{
-				if(s[j - i] == s[j])
-				{
-					z[i] = j - i + 1;
-					l = i;
-					r = j;
-				}
-				else break;
-			}
-		}
-		else if(z[i - l] <= r - i) z[i] = z[i - l];
-		else
-		{
-			z[i] = r - i + 1;
-			l = i;
-			for(int j = r + 1; j < s.size(); j++)
-			{
-				if(s[j - l] == s[j])
-				{
-					z[i] = j - i + 1;
-					r = j;
-				}
-				else break;
-			}
-		}
-	}
+    int l = 0, r = 0;
+    for(int i = 1; i < s.size(); i++)
+    {
+        if(r < i)
+        {
+            for(int j = i; j < s.size(); j++)
+            {
+                if(s[j - i] == s[j])
+                {
+                    z[i] = j - i + 1;
+                    l = i;
+                    r = j;
+                }
+                else break;
+            }
+        }
+        else if(z[i - l] <= r - i) z[i] = z[i - l];
+        else
+        {
+            z[i] = r - i + 1;
+            l = i;
+            for(int j = r + 1; j < s.size(); j++)
+            {
+                if(s[j - l] == s[j])
+                {
+                    z[i] = j - i + 1;
+                    r = j;
+                }
+                else break;
+            }
+        }
+    }
 
-	z[0] = s.size();
+    z[0] = s.size();
 }
 
 int main()
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     
     getline(cin, s);
-	return 0;
+    return 0;
 }
